5024ea6ffc87108a8c684040e70ac26baff3d5a4
aroraabhishek67 <aroraabhishek67@gmail.com>
2019-01-17 21:36:24 +0530

    first commit

---
+++ src/main/Faltoo.java
@@ -0,0 +1,44 @@
+package main;
+import java.util.Arrays;
+public class Faltoo
+{
+    int[] list = new int[10];
+
+    public static void main(String[] args)
+    {
+
+        Faltoo l1 = new Faltoo();
+
+        System.out.println(Arrays.toString(l1.list));
+
+        for (int i = 0; i < 10; i++)
+        {
+            l1.insert(42 + i);
+        }
+
+        l1.remove(0);
+
+        System.out.println(Arrays.toString(l1.list));
+    }
+
+    void insert(int value)
+    {
+        for (int i = 0; i < list.length; i++)
+        {
+            if (list[i] == 0)
+            {
+                list[i] = value;
+                break;
+            }
+        }
+    }
+
+    void remove(int index)
+    {
+        for (int i = index; i < list.length - 1; i++)
+        {
+            list[i] = list[i + 1];
+        }
+        list[list.length - 1] = 0;
+    }
+ }
